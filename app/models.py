244b0c975bddc3b93e4a6e7cc7e95f3a35d47739
Vitor Kayron <vitork2016@gmail.com>
2023-07-11 10:02:47 -0300

    Add Product Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100, blank=False, null=True)
+    valor = models.DecimalField(max_digits=1000, decimal_places=3) 
+    quantidade_estoque = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.nome
--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+# def novo_produto(request):
+#     if request.method == "POST":
