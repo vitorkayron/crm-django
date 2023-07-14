468f7049d0bea1cbfa94eb73e38a71bea25cda12
Vitor Kayron <vitork2016@gmail.com>
2023-07-14 10:24:44 -0300

    Add Update and Delete Products

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('novo_produto/', views.novo_produto, name='novo_produto')
+    path('novo_produto/', views.novo_produto, name='novo_produto'),
+    path('deletar_produto/<int:id>', views.deletar_produto, name='deletar_produto'),
+    path('atualizar_produto/<int:id>', views.atualizar_produto, name='atualizar_produto')
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from .forms import Novo_Produto
+from .models import Produto
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 # def novo_produto(request):
 #     if request.method == "POST":
 
@@ -19,3 +20,25 @@ def novo_produto(request):
             return redirect('/')
     else:
         return render(request, 'novo_produto.html', {'form': form})
+    
+def deletar_produto(request, id):
+    produto = Produto.objects.get(id=id)
+
+    if request.method == 'POST':
+        produto.delete()
+        return redirect('/')
+
+    return render(request,'deletar_produto.html', {'produto': produto})
+
+def atualizar_produto(request, id):
+    produto = Produto.objects.get(id=id)
+     
+    if request.method == "POST":
+        form = Novo_Produto(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = Novo_Produto(instance=produto)
+
+    return render(request, 'atualizar_produto.html', {'produto': produto, 'form': form})
